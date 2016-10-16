858dd16900a8ccea0865f837cea607938404046d
Thomas Sanchez <thomas.sanchez@epfl.ch>
2016-10-16 21:11:19 +0200

    Commented + fixed the code + (added a semester in the duration for the students with 0 in MS1, MS2)

--- 02 - Data from the Web/helpers.py
+++ 02 - Data from the Web/helpers.py
@@ -1,4 +1,13 @@
 def getFullUrl(PeriodeAcad, PeriodePedag):
+    """
+    Given an academic period (years) and a pedagogic period (bachelor semester), 
+    fills the general URL with the values associated to those parameters.
+    
+    @param PeriodeAcad :  Academic year string, in the form ('2015-2016')
+    @param PeriodePedag : Pedagogic period string, in the form ('Bachelor x')
+    @return url :         The complete url associated to those period, from which we can fetch the students.
+    
+    """
     url = base_url
     url = url.replace('[UNITE_ACADEMIQUE]', str(url_section))
     url = url.replace('[PERIODE_ACADEMIQUE]', str(url_years[PeriodeAcad]))
